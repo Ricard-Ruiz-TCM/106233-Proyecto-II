6725b8fc72a01dac14a099b3e4682c2c78d111e4
Ricard Ruiz <rruizc@edu.tecnocampus.cat>
2022-05-21 01:41:25 +0200

    Audio & 2 efectos para probar :3

--- Assets/Scripts/Music and FX/MusicPlayer.cs
+++ Assets/Scripts/Music and FX/MusicPlayer.cs
@@ -1,29 +1,61 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Audio;
+using System.Collections.Generic;
 
-public class MusicPlayer : MonoBehaviour
-{
+public class MusicPlayer : MonoBehaviour {
+
+    /////////////////////////////////////////////////////////////////////
+    // Singleton Instance
+    public static MusicPlayer Instance { get; private set; }
+    void Awake(){
+        if (Instance != null && Instance != this) Destroy(this);
+                                             else Instance = this;
+    }
+    /////////////////////////////////////////////////////////////////////
+
     public GameObject objectMusic;
     AudioSource audioSurce;
     private float MusicVolume = 0;
-    public List<AudioClip> songs;
 
-    private void Awake()
-    {
+    // Diccionario de FX
+    public List<AudioClip> _fx;
+    public Dictionary<string, int> _fxDic;
+
+    // Diccionario de Musica
+    public List<AudioClip> _music;
+    public Dictionary<string, int> _musicDic;
+
+    // Unity
+    void Start(){
         audioSurce.Play();
         audioSurce.volume = MusicVolume;
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
         audioSurce = objectMusic.GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    // MusicPlayer.cs <Load & Store>
+    private AudioClip Load(string audio){
+        return null;
     }
+
+    private bool Exists(string audio){
+        return false;
+    }
+
+
+    // MusicPlayer.cs <Play>
+    public void PlayFX(string audio, float volume, bool repeat){
+        if (!Exists(audio)) Load(audio);
+        ////////////////////////////////
+        // Play del FX con el mixer del FX
+    }
+
+    public void PlayMusic(string audio, float volume, bool repeat){
+        if (!Exists(audio)) Load(audio);
+        ////////////////////////////////
+        // Play de la Music con el mixer de la Music
+    }
+
+    public bool IsPlaying(string audio){
+        return false;
+    }
+
 }
