52af4bb60e0985bff456479d70898d0e1685adba
Ricard Ruiz <rruizc@edu.tecnocampus.cat>
2022-05-22 01:23:40 +0200

    Movimiento de la cámara 2.0

--- Assets/Scripts/Camera/CameraMovement.cs
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -2,19 +2,37 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour {
 
-    private Camera _camera;
+    // Target, aka player
     private Transform _player;
+    private Rigidbody2D _player_body;
 
+    // Camera Movement controll
     [SerializeField]
-    private Vector3 _offset;
+    private float _deltaX;
+    [SerializeField]
+    private float _smooth;
 
+    // Temp vector3 for next pos
+    [SerializeField]
+    Vector3 _nextPos;
+
+    // Unity
     void Start() {
-        _camera = GetComponent<Camera>();
         _player = GameObject.FindObjectOfType<Player>().gameObject.transform;
+        _player_body = _player.gameObject.GetComponent<Rigidbody2D>();
+
+        _deltaX = 1.25f;
+        _smooth = 1.0f;
     }
 
+    // Unity
     void Update() {
-        _camera.transform.position = new Vector3(_player.position.x, _player.position.y, _camera.transform.position.z);
+        _nextPos = new Vector3(_player.position.x, _player.position.y, transform.position.z);
+
+        _nextPos.x += (_deltaX * _player.transform.right.x);
+        _nextPos.y += (_player_body.velocity.y / 2.0f);
+
+        transform.position = Vector3.Lerp(transform.position, _nextPos, _smooth * Time.deltaTime);
     }
 
 }
