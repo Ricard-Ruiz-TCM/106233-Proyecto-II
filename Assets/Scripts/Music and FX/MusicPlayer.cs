277effbca52b71ef17d583e19013ddaea3f9a216
Ricard Ruiz <rruizc@edu.tecnocampus.cat>
2022-05-21 02:23:35 +0200

    cosas del audio para audio 3d :3

--- Assets/Scripts/Music and FX/MusicPlayer.cs
+++ Assets/Scripts/Music and FX/MusicPlayer.cs
@@ -12,10 +12,6 @@ public class MusicPlayer : MonoBehaviour {
     }
     /////////////////////////////////////////////////////////////////////
 
-    public GameObject objectMusic;
-    AudioSource audioSurce;
-    private float MusicVolume = 0;
-
     // Diccionario de FX
     public List<AudioClip> _fx;
     public Dictionary<string, int> _fxDic;
@@ -26,34 +22,69 @@ public class MusicPlayer : MonoBehaviour {
 
     // Unity
     void Start(){
-        audioSurce.Play();
-        audioSurce.volume = MusicVolume;
-        audioSurce = objectMusic.GetComponent<AudioSource>();
+        _fx = new List<AudioClip>();
+        _fxDic = new Dictionary<string, int>();
+
+        _music = new List<AudioClip>();
+        _musicDic = new Dictionary<string, int>();
     }
 
-    // MusicPlayer.cs <Load & Store>
-    private AudioClip Load(string audio){
-        return null;
+    // MusicPlayer.cs <Load>
+    private void Load(List<AudioClip> list, Dictionary<string, int> dic, string path, string audio){
+        list.Add(Resources.Load<AudioClip>("Audio/" + path + "/" + audio));
+        dic.Add(audio, list.Count - 1);
     }
 
-    private bool Exists(string audio){
-        return false;
+    private void LoadFX(string audio){
+        Load(_fx, _fxDic, "FX", audio);
     }
 
+    private void LoadMusic(string audio) {
+        Load(_music, _musicDic, "Music", audio);
+    }
+
+    // MusicPlayer.cs <Exists>
+    private bool Exists(Dictionary<string,int> dic, string key) {
+        return dic.ContainsKey(key);
+    }
+
+    private bool ExistsFX(string audio){
+        return Exists(_fxDic, audio);
+    }
+
+    private bool ExistsMusic(string audio){
+        return Exists(_musicDic, audio);
+    }
+
+    // MusicPlayer.cs <Get>
+    private AudioClip GetFX(string audio){
+        return _fx[_fxDic[audio]];
+    }
+
+    private AudioClip GetMusic(string audio){
+        return _fx[_fxDic[audio]];
+    }
 
     // MusicPlayer.cs <Play>
+    public void PlayMe(AudioSource audio){
+        audio.PlayOneShot(audio.clip);
+    }
+
     public void PlayFX(string audio, float volume, bool repeat){
-        if (!Exists(audio)) Load(audio);
+        if (!ExistsFX(audio)) LoadFX(audio);
         ////////////////////////////////
         // Play del FX con el mixer del FX
+        GetFX(audio);
     }
 
     public void PlayMusic(string audio, float volume, bool repeat){
-        if (!Exists(audio)) Load(audio);
+        if (!ExistsMusic(audio)) LoadMusic(audio);
         ////////////////////////////////
         // Play de la Music con el mixer de la Music
+        GetMusic(audio);
     }
 
+    // MusicPlayer.cs <Control>
     public bool IsPlaying(string audio){
         return false;
     }
