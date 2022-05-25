b880a7057897bf6b0eea424aaae849575e540189
Ricard Ruiz <rruizc@edu.tecnocampus.cat>
2022-05-25 20:31:51 +0200

    prefabs de la manos, ajustes del cartel, etc

---
+++ Assets/Scripts/UI/HandShakerUI.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class HandShakerUI : MonoBehaviour {
+
+    [SerializeField]
+    private float _timeAlive;
+    private float _time;
+
+    public float _speed;
+    public float _rack;
+
+    void Awake(){
+        _timeAlive = 1.5f;
+        _time = 0.0f;
+        _speed = 0.03f;
+        _rack = 20.0f;
+    }
+
+    // Unity
+    void Start() {
+        Destroy(this.gameObject, _timeAlive);
+    }
+
+    void Update() {
+        _time += Time.deltaTime;
+        transform.position += new Vector3(Mathf.Cos(_time * _rack) * _speed, -0.005f, 0.0f);
+    }
+}
