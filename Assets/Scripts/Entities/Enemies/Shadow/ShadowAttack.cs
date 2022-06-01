b28a55be1d8e9f34365152b672d53df08f05b2f4
Veronika Bolshakova <veronikabox@gmail.com>
2022-06-01 17:29:08 +0200

    Cosas

--- Assets/Scripts/Entities/Enemies/Shadow/ShadowAttack.cs
+++ Assets/Scripts/Entities/Enemies/Shadow/ShadowAttack.cs
@@ -15,21 +15,25 @@ public class ShadowAttack : EnemyCombat
     private Animator animator;
     private PlayerCombat _playerCombat;
     private Transform player;
+    public bool attacked = false;
     // Start is called before the first frame update
     void Start()
     {
         shadowAI = GetComponentInParent<ShadowAI>();
         animator = gameObject.GetComponentInParent<Animator>();
         _health = 20;
+        animator.SetBool("Idle", true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(shadowAI.Detected)
+        /*if(shadowAI.Detected)
         {
             ShadowAttacks();
-        }
+        }*/
+
+
 
         if (_health <= 0f)
         {
@@ -44,9 +48,10 @@ public class ShadowAttack : EnemyCombat
         return dist < DetectionRange;
     }
 
-    void ShadowAttacks()
+    public void ShadowAttacks()
     {
-        shadow.AddForce(new Vector2(0, transform.up.y * 10.0f));
+        //shadow.AddForce(new Vector2(0, transform.up.y * 10.0f));
+        Debug.Log("Shadow attacks");
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -54,7 +59,9 @@ public class ShadowAttack : EnemyCombat
         var player = collision.gameObject.GetComponent<PlayerCombat>();
         if (collision.collider.gameObject.tag == "Player")
         {
-            shadow.velocity = Vector2.zero;
+            player.TakeDamage(currentAttack);
+            attacked = true;
+            /*shadow.velocity = Vector2.zero;
             shadow.angularVelocity = 0.0f;
 
             if (transform.right.x == -1.0f)
@@ -74,7 +81,7 @@ public class ShadowAttack : EnemyCombat
                 force.Normalize();
                 GetComponent<Rigidbody2D>().AddForce(-forceNew * magnitude);
                 player.TakeDamage(currentAttack);
-            }
+            }*/
 
         }
     }
