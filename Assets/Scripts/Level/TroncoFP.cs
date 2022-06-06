4773718003df7776ba8bbedcdfa414d8f2fc536e
Ricard Ruiz <rruizc@edu.tecnocampus.cat>
2022-06-06 17:55:23 +0200

    Instnaciador de particulas y test

---
+++ Assets/Scripts/Level/TroncoFP.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+
+public class TroncoFP : MonoBehaviour
+{
+
+    // Fall & Fade Control
+    private bool _fall;
+    [SerializeField]
+    private float _fallTime;
+    private bool IsFalling() { return _fall; }
+    private bool _fade;
+    [SerializeField]
+    private float _fadeTime;
+    private bool IsFaded() { return _fade; }
+    private bool CanFade() { return !IsFaded(); }
+
+    // InitPos
+    private Vector2 _initialPos;
+
+    // Unity
+    void Awake()
+    {
+        _fall = false;
+        _fade = false;
+        _initialPos = this.transform.position;
+    }
+
+    // Unity
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision == null) return;
+        if (collision.gameObject.tag != "Player") return;
+
+        if (CanFade()) Fade();
+    }
+
+    // FallingPlatform.cs
+    private void Fade()
+    {
+        if (IsFaded()) return;
+        _fade = true;
+        GetComponent<SpriteRenderer>().color = new Color(1.0f, 1.0f, 1.0f, 0.5f);
+        Invoke("Fall", _fadeTime);
+    }
+
+    private void Fall()
+    {
+        if (IsFalling()) return;
+        _fall = true;
+        ParticleInstancer.Instance.StartParticles("Tronco", transform);
+        GetComponent<Rigidbody2D>().isKinematic = false;
+        GetComponent<BoxCollider2D>().isTrigger = true;
+        Invoke("Restore", _fallTime);
+    }
+
+    private void Restore()
+    {
+        _fall = false;
+        _fade = false;
+        GetComponent<BoxCollider2D>().isTrigger = false;
+        GetComponent<Rigidbody2D>().isKinematic = true;
+        GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        GetComponent<SpriteRenderer>().color = new Color(1.0f, 1.0f, 1.0f, 1.5f);
+        transform.position = _initialPos;
+    }
+
+}
---
+++ Assets/Scripts/ParticleInstancer.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleInstancer : MonoBehaviour
+{
+
+    /////////////////////////////////////////////////////////////////////
+    // Singleton Instance
+    public static ParticleInstancer Instance { get; private set; }
+    void Awake()
+    {
+        if (Instance != null && Instance != this) Destroy(this);
+        else Instance = this;
+        _particles = new Dictionary<string, GameObject>();
+    }
+    /////////////////////////////////////////////////////////////////////
+
+    private Dictionary<string, GameObject> _particles;
+
+    private void LoadParticles(string name)
+    {
+        _particles.Add(name, Resources.Load<GameObject>("Prefabs/Particles/" + name));
+    }
+
+    private bool Exists(string name)
+    {
+        return _particles.ContainsKey(name);
+    }
+
+    public void StartParticles(string name, Transform parent)
+    {
+        if (!Exists(name)) LoadParticles(name);
+        InstanceParticles(name, parent);
+    }
+
+    private void InstanceParticles(string name, Transform parent)
+    {
+        Instantiate(_particles[name], parent);
+    }
+
+
+}
