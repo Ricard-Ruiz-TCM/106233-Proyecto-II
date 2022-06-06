2c6d17b9f79e10de6cc2096c376c2e2a27d853b1
Ricard Ruiz <rruizc@edu.tecnocampus.cat>
2022-06-06 20:14:54 +0200

    ShadowIntensity

--- Assets/ShadowIntensity.cs
+++ Assets/ShadowIntensity.cs
@@ -7,13 +7,13 @@ public class ShadowIntensity : MonoBehaviour {
 
     void Start() {
         _light = GetComponent<Light2D>();
-        InvokeRepeating("ChangeSI", 0.0f, 0.15f);
+        InvokeRepeating("ChangeSI", 0.0f, 0.25f);
     }
 
     private void ChangeSI()
     {
-        _light.shadowIntensity += Random.Range(-0.05f, 0.05f);
-        _light.shadowIntensity = Mathf.Min(Mathf.Max(_light.shadowIntensity, 0.0f), 0.25f);
+        _light.shadowIntensity += Random.Range(-0.005f, 0.005f);
+        _light.shadowIntensity = Mathf.Min(Mathf.Max(_light.shadowIntensity, 0.05f), 0.20f);
     }
 
     public void StopSunshines()
