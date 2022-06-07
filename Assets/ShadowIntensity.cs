b28d270f8fa8cbebcf15fa3e23b37d7cfee81f4f
Ricard Ruiz <rruizc@edu.tecnocampus.cat>
2022-06-07 12:15:03 +0200

    Canvas Fix + ShadowI

--- Assets/ShadowIntensity.cs
+++ Assets/ShadowIntensity.cs
@@ -17,7 +17,7 @@ public class ShadowIntensity : MonoBehaviour {
     private void ChangeSI()
     {
         _light.shadowIntensity += Random.Range(_increasing / 10.0f, _increasing);
-        _light.shadowIntensity = Mathf.Clamp(_light.shadowIntensity, 0.005f, 0.15f);
+        _light.shadowIntensity = Mathf.Clamp(_light.shadowIntensity, 0.005f, 0.1f);
         _times++;
         if (Random.Range(0, 200) < (_times / 2.5f)) { _increasing *= -1; _times = 0; }
     }
