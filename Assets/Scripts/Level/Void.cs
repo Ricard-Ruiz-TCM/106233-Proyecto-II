8657e3b7b57294c0da1fc338f09fdb3d2fed0aab
Ricard Ruiz <rruizc@edu.tecnocampus.cat>
2022-06-12 23:49:03 +0200

    Fix de la muerte por caída

--- Assets/Scripts/Level/Void.cs
+++ Assets/Scripts/Level/Void.cs
@@ -2,11 +2,13 @@ using UnityEngine;
 
 public class Void : MonoBehaviour {
     
+
+
     // Unity
     private void OnTriggerEnter2D(Collider2D collision){
         if (collision.gameObject == null) return;
         if (collision.gameObject.tag != "Player") return;
 
-        collision.gameObject.GetComponent<Player>().TakeDamage(100, DEATH_CAUSE.D_FALL);
+        collision.gameObject.GetComponent<Player>().TakeDamage(100, DEATH_CAUSE.D_FALL, true);
     }
 }
