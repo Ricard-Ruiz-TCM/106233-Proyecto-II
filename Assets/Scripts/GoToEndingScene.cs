d58904e757e570587947fb0a443cbb428a86bfb8
jordi borraz giribet <jordiborraz@gmail.com>
2022-06-13 22:21:05 +0200

    Cinematicas cambio escenas

---
+++ Assets/Scripts/GoToEndingScene.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToEndingScene : MonoBehaviour
+{
+    [SerializeField]
+    private string sceneNameToLoad;
+    private float timeElapsed;
+    [SerializeField]
+    private float delayBeforeLoaading;
+    private bool startTimer;
+    public ParticleSystem confetti1;
+    public ParticleSystem confetti2;
+
+    private void OnEnable()
+    {
+        BossAttack.OnBossDown += onNextScene;
+    }
+    private void OnDisable()
+    {
+        BossAttack.OnBossDown -= onNextScene;
+
+    }
+    private void Update()
+    {
+        if (startTimer == false) return;
+        if (timeElapsed > delayBeforeLoaading)
+        {
+            SceneManager.LoadScene(sceneNameToLoad);
+        }
+        timeElapsed += Time.deltaTime;
+
+    }
+    private void Start()
+    {
+        startTimer = false;
+        timeElapsed = 0;
+    }
+    private void onNextScene()
+    {
+        startTimer = true;
+        confetti1.Play();
+        confetti2.Play();
+    }
+}
+
