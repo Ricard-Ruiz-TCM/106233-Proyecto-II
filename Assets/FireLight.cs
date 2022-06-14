e70879969d34d8da43930620bab4494f75728078
Ricard Ruiz <rruizc@edu.tecnocampus.cat>
2022-06-14 20:50:22 +0200

    ajustes de lvl y luces 3

--- Assets/FireLight.cs
+++ Assets/FireLight.cs
@@ -16,6 +16,7 @@ public class FireLight : MonoBehaviour {
     private void ChangeFallOff(){
         _fallOff = Random.Range(85.0f, 100.0f) / 100.0f;
         _light.intensity = _fallOff;
+        _light.color = new Color(1.0f, (Random.Range(90.0f, 110.0f) / 255.0f), 0.0f, 1.0f);
         Invoke("ChangeFallOff", 0.05f);
     }
 
